fc603fc15ce7afafce969be9a2e6a203a7002a7d
Vishnu Pandaraparambath <lfjt0879@leeds.ac.uk>
2026-02-22 22:58:00 +0000

    done ws task

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,7 +1,7 @@
 
 /*
-Name:
-Student ID:
+Name: Vishnu Pandaraparambath
+Student ID: 201923075
 */
 
 #include <stdio.h>
@@ -10,10 +10,25 @@ Student ID:
 int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
+    char result[60];
 
+    for(int k = 1; k < argc; k++){
+        // Ensuring the last character doesnt end with a hyphen
+        if(k != argc - 1){
+            char temp[10];
+            strcpy(temp, argv[k]);
+            strcat(temp, "-");
+            strcat(result, temp);
+        }
+        //just adds the last character without the hyphen
+        else{
+            strcat(result, argv[k]);
+        }
+    }
     // process the command-line data using appropriate string functions
 
-    printf("%s\n",buffer); // use only this single print statement in your submitted code
+    printf("%s\n",result); // use only this single print statement in your submitted code
 
     return 0;
 }
+
